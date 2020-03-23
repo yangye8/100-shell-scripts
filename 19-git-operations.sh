67ea7c7bef407c396986cd7e9eff0afe3c950e97
Ye Yang <yangye363@hotmail.com>
2020-03-23 20:47:45 +0800

    update
    Signed-off-by: Ye Yang <yangye363@hotmail.com>

--- 19-git-operations.sh
+++ 19-git-operations.sh
@@ -1,4 +1,9 @@
-command list
-
+# set-url
 git remote set-url origin git@github.com:yangye8/100-shell-scripts.git
+
+#clean .git
+git rev-list --objects --all | grep "$(git verify-pack -v .git/objects/pack/*.idx | sort -k 3 -n | tail -5 | awk '{print$1}')"
+git filter-branch -f --prune-empty --index-filter 'git rm -rf --cached --ignore-unmatch <file>' --tag-name-filter cat -- --all
+git filter-branch -f --prune-empty --index-filter 'git rm -rf --cached --ignore-unmatch <file>' --tag-name-filter cat -- --all
+git push origin --force --all
 
